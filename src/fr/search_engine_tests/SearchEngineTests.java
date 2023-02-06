87e14a4cbefd879043af0774a56daf66c2c4e051
Ti21l <116551674+Ti21l@users.noreply.github.com>
2023-02-06 14:40:12 +0100

    Update SearchEngineTests.java

--- src/fr/search_engine_tests/SearchEngineTests.java
+++ src/fr/search_engine_tests/SearchEngineTests.java
@@ -1,5 +1,13 @@
 package fr.search_engine_tests;
 
-public class SearchEngineTests {
+import fr.search_engine.IndexedPage;
 
+public class SearchEngineTests {
+	
+	public static void main(String[] args) { 
+	String[] lines = {"http://example.org", "hello:1", "world:1", "java:7"};
+	IndexedPage indexedPageTest = new IndexedPage(lines);
+	
+	System.out.println(indexedPageTest.getNorm());
+	}
 }
