793e82e36df246b66e5882c3985f0dc290813504
Santhakumaran Akira <75495075+Akira98000@users.noreply.github.com>
2023-02-06 14:52:57 +0100

    New main with comments
    I have update the main with the test arguments for user:@thomasbres & my test 
    This have to be used to check if Indexed work and SearchEngineResult work

--- src/fr/search_engine_tests/SearchEngineTests.java
+++ src/fr/search_engine_tests/SearchEngineTests.java
@@ -1,13 +1,46 @@
+/*
+ * @author : akira
+ * date : 06/02/2023 
+ */
+
 package fr.search_engine_tests;
 
+/* Package Source : fr.search_engine_test 
+ * IndexedPage.java
+ * SearchEngine.java
+ * SearchResult.java
+ */ 
+
 import fr.search_engine.IndexedPage;
+import fr.search_engine.SearchResult; 
+// Import never used  :   !! We have to do the modif 
+import fr.search_engine.SearchEngine;
 
 public class SearchEngineTests {
 	
-	public static void main(String[] args) { 
-	String[] lines = {"http://example.org", "hello:1", "world:1", "java:7"};
-	IndexedPage indexedPageTest = new IndexedPage(lines);
-	
-	System.out.println(indexedPageTest.getNorm());
+	public static void main(String[] args) {
+		// Test for user : @akirasanthakumaran || File Name : SearchResult.java : 
+		SearchResult page1 = new SearchResult(args[0]);
+		System.out.println("Degre de similaritÃ© : 0 ");
+		page1.toString();
+		
+		// Test for user : @thomasbres || File Name : IndexedPage.java
+		String[] lines = {"http://example.org", "hello:1", "world:1", "java:7"};
+		IndexedPage indexedPageTest = new IndexedPage(lines);
+		System.out.println(indexedPageTest.getNorm());
+		
+		
+		// Final Main :
+		/*public static void main(String[] args) {
+			  IndexedPage page1 = new IndexedPage(new String[] {"http://fr.example.org", "hello:10", "world:5"});
+			  System.out.println(page1);
+			  System.out.println(page1.getPonderation("hello")); 
+			  System.out.println(page1.getPonderation("other"));
+			  IndexedPage page2 = new IndexedPage(new String[] {"http://fr.example2.org", "hello:5", "france:2"});
+			  System.out.println("Degre de similarite des deux pages: " + page1.proximity(page2));
+		}*/
+		
 	}
+
 }
+
